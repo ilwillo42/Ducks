d6d70d96ac445a16a0a2e4603772745331d01284
William Franchini <william@quarantadue.io>
2022-05-13 00:55:52 +0200

    Add project files.

---
+++ Ducks/Duck.cs
@@ -0,0 +1,90 @@
+﻿  
+
+namespace Ducks
+{
+    public  abstract class Duck
+    {
+        public virtual Duck Quack() { Console.WriteLine("quack"); return this; }
+        public virtual Duck Fly() { Console.WriteLine("fly"); return this; }
+        public virtual Duck Figth() { Console.WriteLine("sdeng!"); return this; }
+        public virtual Duck Eat() { Console.WriteLine("gnam"); return this; }
+
+    }
+
+    public class CountryDuck : Duck
+    {
+        public override Duck Quack() { Console.WriteLine("keck"); return this; }
+        public override Duck Fly() { Console.WriteLine("fly"); return this; }
+        public override Duck Figth() { Console.WriteLine("sdeng!"); return this; }
+        public override Duck Eat() { Console.WriteLine("gnam gnam gnam"); return this; }
+
+    }
+
+    public class CityDuck : Duck
+    {
+        public override Duck Fly() { Console.WriteLine("jump"); return this; }
+        public override Duck Figth() { Console.WriteLine("I won't fly"); return this; }
+        public override Duck Eat() { Console.WriteLine("gnam"); return this; }
+    }
+
+
+
+    public class MountainDuck : Duck
+    {
+        public override Duck Quack() { Console.WriteLine("keck"); return this; }
+        public override Duck Fly() { Console.WriteLine("high fly"); return this; }
+        public override Duck Figth() { Console.WriteLine("I won't figth"); return this; }
+        public override Duck Eat() { Console.WriteLine("gnam gnam gnam"); return this; }
+
+    }
+
+    public class RubberDuck : Duck
+    {
+        public override Duck Quack() { Console.WriteLine("fake quaack"); return this; }
+        public override Duck Fly() { Console.WriteLine("I don't fly"); return this; }
+        public override Duck Figth() { Console.WriteLine("I won't figth"); return this; }
+        public override Duck Eat() { Console.WriteLine("I don't eat"); return this; }
+
+    }
+
+    public class CrazyDuck : Duck
+    {
+        public override Duck Quack() { Console.WriteLine("quaakskdkakkkkakadquakka"); return this; }
+        public override Duck Fly() { Console.WriteLine("high fly"); return this; }
+        public override Duck Figth() { Console.WriteLine("Seek and destroy"); return this; }
+        public override Duck Eat() { Console.WriteLine("gnam"); return this; }
+
+    }
+
+    public class BorgDuck : Duck
+    {
+        public override Duck Quack() { Console.WriteLine("You will be assimilated.quack Resistance is futile.quack"); return this; }
+        public override Duck Fly() { Console.WriteLine("jet fly"); return this; }
+        public override Duck Figth() { Console.WriteLine("Seek and destroy"); return this; }
+        public override Duck Eat() { Console.WriteLine("I don't eat"); return this; }
+
+    }
+
+    public class CloudDuck : Duck
+    {
+        public override Duck Quack() { Console.WriteLine("quaakskdkakkkkakadquakka"); return this; }
+        public override Duck Fly() { Console.WriteLine("jet fly"); return this; }
+        public override Duck Figth() { Console.WriteLine("I won't figth"); return this; }
+        public override Duck Eat() { Console.WriteLine("gnam gnam gnam"); return this; }
+
+    }
+
+    public class LazyDuck : Duck
+    {
+        public override Duck Quack() { Console.WriteLine("quack"); return this; }
+        public override Duck Fly() { Console.WriteLine("fly"); return this; }
+        public override Duck Figth() { Console.WriteLine("I won't figth"); return this; }
+        public override Duck Eat() { Console.WriteLine("gnam"); return this; }
+    }
+
+
+}
+
+
+
+
---
+++ Ducks/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Ducks;
+
+Console.WriteLine("Hello, Duck!");
+var manyducks = new List<Duck>() { new MountainDuck(), new CityDuck(), new CloudDuck(), new BorgDuck(), new CrazyDuck(), new LazyDuck() };
+foreach (var duck in manyducks)
+{
+    Console.WriteLine(duck.GetType().Name);
+    Console.WriteLine($"--------------------");
+    duck.Eat().Fly().Figth().Quack();
+    Console.WriteLine($"--------------------");
+}
